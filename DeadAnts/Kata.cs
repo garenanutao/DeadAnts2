a4ad092221c48a33e529d1dd841a30a00486a09e
Sheng Fu,Ke <w91090@gmail.com>
2019-01-06 18:16:10 +0800

    Class to DeadAnt

---
+++ DeadAnts/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadAnts
+{
+    public class Kata
+    {
+        public static int DeadAntCount(string ants)
+        {
+            int deadAntNum = 0;
+            string deadAnts;
+            if (ants == null) return 0;
+            string[] antGroups = ants.Split(new char[] { ' ', '.' });
+            foreach (string ant in antGroups)
+            {
+                deadAnts = ant.Replace("ant", "").Trim();
+                if (deadAnts != "")
+                {
+                    foreach (char antHead in deadAnts)
+                    {
+                        if (antHead == 'a') deadAntNum++;
+                    }
+                }
+            }
+            return deadAntNum;
+            return 0;
+        }
+    }
+}
---
+++ DeadAntsTests1/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeadAnts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadAnts.Solution
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void DeadAntCountBasicTests()
+        {
+            Assert.AreEqual(0, Kata.DeadAntCount("ant ant ant ant"));
+            Assert.AreEqual(0, Kata.DeadAntCount(null));
+            Assert.AreEqual(2, Kata.DeadAntCount("ant anantt aantnt"));
+            Assert.AreEqual(1, Kata.DeadAntCount("ant ant .... a nt"));
+        }
+    }
+}
